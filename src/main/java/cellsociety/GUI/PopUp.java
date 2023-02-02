ced85e4dcecdc18cb0f9cf96f31f991f85668d39
Han Zhang <hz244@duke.edu>
2023-02-02 14:59:41 -0500

    PopUp can take List of String issues and display textbox and text pairs, with two buttons on bottom

--- src/main/java/cellsociety/GUI/PopUp.java
+++ src/main/java/cellsociety/GUI/PopUp.java
@@ -2,21 +2,39 @@ package cellsociety.GUI;
 
 import java.util.List;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PopUp {
 
-  Stage popUpStage;
+  private static Stage popUpStage;
   public PopUp(List<String> Issues){
     popUpStage = new Stage();
     VBox comp = new VBox();
 
+    TextField issueField;
+    Text issueText;
+
+    Button clear = new Button("Clear");
+    Button submit = new Button("Submit");
+
     for(String issue:Issues){
-      comp.getChildren().add(new TextField(issue));
+      HBox inside = new HBox();
+      issueField = new TextField();
+      issueText = new Text(issue);
+      inside.getChildren().addAll(issueText, issueField);
+      comp.getChildren().add(inside);
     }
+
+    comp.getChildren().add(new HBox(clear, submit));
     Scene stageScene = new Scene(comp, 300, 300);
     popUpStage.setScene(stageScene);
+  }
+  public void showPopUp(){
+    popUpStage.show();
   }
 }
