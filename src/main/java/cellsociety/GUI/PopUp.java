1eebc1a4130b71482c64f70a6a3a4aaba175a3f1
Han Zhang <hz244@duke.edu>
2023-02-02 15:35:59 -0500

    Figure out format and list output with XMLConfig class, as well as timing

--- src/main/java/cellsociety/GUI/PopUp.java
+++ src/main/java/cellsociety/GUI/PopUp.java
@@ -1,6 +1,11 @@
 package cellsociety.GUI;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -12,6 +17,9 @@ import javafx.stage.Stage;
 public class PopUp {
 
   private static Stage popUpStage;
+  private static Set<TextField> TextFields = new HashSet<>();
+
+  private static List<String> UserInput= new ArrayList<>();
   public PopUp(List<String> Issues){
     popUpStage = new Stage();
     VBox comp = new VBox();
@@ -26,9 +34,28 @@ public class PopUp {
       HBox inside = new HBox();
       issueField = new TextField();
       issueText = new Text(issue);
+      TextFields.add(issueField);
       inside.getChildren().addAll(issueText, issueField);
       comp.getChildren().add(inside);
     }
+    clear.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        for(TextField field: TextFields){
+          field.clear();
+        }
+      }
+    });
+
+    submit.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        for(TextField field: TextFields){
+          UserInput.add(field.getText());
+        }
+        returnInput();
+      }
+    });
 
     comp.getChildren().add(new HBox(clear, submit));
     Scene stageScene = new Scene(comp, 300, 300);
@@ -36,5 +63,11 @@ public class PopUp {
   }
   public void showPopUp(){
     popUpStage.show();
+  }
+
+  //TODO, properly return after discussion with Changmin
+  public static List<String> returnInput(){
+    System.out.println(UserInput);
+    return UserInput;
   }
 }
