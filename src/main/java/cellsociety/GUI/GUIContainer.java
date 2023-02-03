24beb598bd20c42b76e14000d0d321269df680bb
Han Zhang <hz244@duke.edu>
2023-02-03 12:23:15 -0500

    Added Slider, deleted Class Slider because Javafx implementation was found

--- src/main/java/cellsociety/GUI/GUIContainer.java
+++ src/main/java/cellsociety/GUI/GUIContainer.java
@@ -2,6 +2,7 @@ package cellsociety.GUI;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -9,19 +10,25 @@ import javafx.stage.Stage;
 public class GUIContainer {
   Stage mainStage;
   GridPane pane;
+
+  Slider slider;
   public GUIContainer(Stage primaryStage) {
     mainStage = primaryStage;
     pane = new GridPane();
 
     pane.setGridLinesVisible(true);
-    Button step = createButton("Step", 0, 4);
-    Button reset = createButton("Reset", 1, 4);
-    Button go = createButton("Go/Pause",2, 4);
-    Button clear = createButton("Clear", 3, 4);
-    Button random = createButton("Random", 4, 4);
+    slider = new Slider();
+    Button step = createButton("Step", 0, 5);
+    Button reset = createButton("Reset", 1, 5);
+    Button go = createButton("Go/Pause",2, 5);
+    Button clear = createButton("Clear", 3, 5);
+    Button random = createButton("Random", 4, 5);
 
     //https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/GridPane.html
     pane.getChildren().addAll(step, reset, go, clear, random);
+    pane.getChildren().add(slider);
+
+
 
     Scene stageScene = new Scene(pane, 1000, 700);
     mainStage.setScene(stageScene);
