beaa246627895104deac1e6af4e309a776872650
Han Zhang <hz244@duke.edu>
2023-02-05 13:53:30 -0500

    Made Grid an Abstract class

--- src/main/java/cellsociety/GUI/Grid.java
+++ src/main/java/cellsociety/GUI/Grid.java
@@ -1,10 +1,21 @@
 package cellsociety.GUI;
 
+import cellsociety.Cell;
+import javafx.scene.layout.GridPane;
+
 /**
  * @Author Han Zhang
  */
 
 
-public class Grid {
+public abstract class Grid {
 
+  int width;
+  int height;
+
+  public Grid(int Width, int Height) {
+    width = Width;
+    height = Height;
+  }
+  public abstract void updateGrid(Cell cell);
 }
