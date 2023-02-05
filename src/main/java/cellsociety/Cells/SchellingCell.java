7922e2242eb76bee330436af3b7e4c475e1e9eb0
Brandon Weiss <brandon.weiss@duke.edu>
2023-02-05 17:49:37 -0500

    Make Schelling model simulation

--- src/main/java/cellsociety/Cells/SchellingCell.java
+++ src/main/java/cellsociety/Cells/SchellingCell.java
@@ -1,5 +1,8 @@
 package cellsociety.Cells;
 
+/*
+ * @author Brandon Weiss
+ */
 public class SchellingCell extends Cell {
     public SchellingCell(int xCoordinate, int yCoordinate) {
         super(xCoordinate, yCoordinate);
