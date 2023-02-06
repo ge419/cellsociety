40b299db767ac7733ad0deef100ac7afe2721ac3
Brandon Weiss <brandon.weiss@duke.edu>
2023-02-06 03:33:57 -0500

    Add randomized starting

--- src/main/java/cellsociety/simulations/Life.java
+++ src/main/java/cellsociety/simulations/Life.java
@@ -1,24 +1,26 @@
 package cellsociety.simulations;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import cellsociety.Cells.Cell;
-import cellsociety.Cells.LifeCell;
 
 /*
  * @author Brandon Weiss
  */
 public class Life extends Simulation {
+    public static final Random RAND_NUM_GEN = new Random();
 
     public Life(String deadString, String aliveString) {
         super(deadString, aliveString);
     }
 
-    public String getUpdatedCellStatus(LifeCell cell, List<Cell> neighbors) {
+    public String getUpdatedCellStatus(Cell cell, List<Cell> neighbors) {
         int alive = countNeighbors(neighbors, getAliveString());
         return toggleCell(cell, alive);
     }
 
-    private String toggleCell(LifeCell cell, int numAlive) {
+    private String toggleCell(Cell cell, int numAlive) {
         if (numAlive == 2) {
             return cell.getStatus();
         }
@@ -26,6 +28,17 @@ public class Life extends Simulation {
             return getAliveString();
         }
         return getDeadString();
+    }
+
+    public Cell randomize(HashMap<String, Double> parameters, int xCoordinate, int yCoordinate) {
+        double alive = parameters.get("perAlive");
+        Cell cell = new Cell(xCoordinate, yCoordinate);
+        if (RAND_NUM_GEN.nextDouble() < alive) {
+            cell.setStatus(getAliveString());
+        } else {
+            cell.setStatus(getDeadString());
+        }
+        return cell;
     }
 
     @Override
