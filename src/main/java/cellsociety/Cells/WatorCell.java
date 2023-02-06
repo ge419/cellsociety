0e5af5218b9173a30f5f3fd83adfdb18a0ca9de2
Brandon Weiss <brandon.weiss@duke.edu>
2023-02-06 03:34:23 -0500

    Simplify cell configuration

--- src/main/java/cellsociety/Cells/WatorCell.java
+++ src/main/java/cellsociety/Cells/WatorCell.java
@@ -4,7 +4,40 @@ package cellsociety.Cells;
  * @author Brandon Weiss
  */
 public class WatorCell extends Cell{
+    private int breedingStage;
+    private int energy;
     public WatorCell(int xCoordinate, int yCoordinate){
         super(xCoordinate, yCoordinate);
+        resetLifeCycle();
+    }
+
+    public void resetLifeCycle(){
+        breedingStage = 0;
+        energy = 0;
+    }
+
+    public boolean reproduce(int breedingTime){
+        if (breedingStage >= breedingTime){
+            return true;
+        }
+        return false;
+    }
+
+    public void move(int xCoordinate, int yCoordinate){
+        energy--;
+        setX(xCoordinate);
+        setY(yCoordinate);
+    }
+
+    public int getEnergy(){
+        return energy;
+    }
+
+    public void setEnergy(int energy){
+        this.energy = energy;
+    }
+
+    public void age(){
+        breedingStage++;
     }
 }
