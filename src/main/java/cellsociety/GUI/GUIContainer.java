10a4778bce5d05ef3861905093c29c0450f7ebd1
Han Zhang <hz244@duke.edu>
2023-02-11 17:29:21 -0500

    Got rid of magic numbers inside of GUI_Container

--- src/main/java/cellsociety/GUI/GUIContainer.java
+++ src/main/java/cellsociety/GUI/GUIContainer.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -45,9 +44,39 @@ public class GUIContainer {
 
   public final static int WINDOW_WIDTH = 1000;
   public final static int WINDOW_HEIGHT = 700;
-
   public final static int DEFAULT_GRID_SIZE = 20;
 
+  //TODO say that decided to use this deisng choice, should be css, but can't figure out how to do it
+  public final static int GRID_COLUMN = 0;
+  public final static int GRID_ROW = 0;
+  public final static int GRID_COLUMN_SPAN = 3;
+  public final static int GRID_ROW_SPAN = 4;
+
+  public final static int DROP_DOWN_COLUMN= 3;
+  public final static int DROP_DOWN_ROW = 2;
+  public final static int DROP_DOWN_COLUMN_SPAN = 2;
+  public final static int DROP_DOWN_ROW_SPAN = 1;
+
+  public final static int FILE_UPLOADER_COLUMN = 3;
+  public final static int FILE_UPLOADER_ROW = 0;
+  public final static int FILE_SAVER_COLUMN = 4;
+  public final static int FILE_SAVER_ROW = 0;
+
+  public final static int DESCRIPTION_BOX_COLUMN = 3;
+  public final static int DESCRIPTION_BOX_ROW = 3;
+  public final static int DESCRIPTION_BOX_COLUMN_SPAN = 2;
+  public final static int DESCRIPTION_BOX_ROW_SPAN = 1;
+
+  public final static int BUTTONS_COLUMN = 0;
+  public final static int BUTTONS_ROW = 5;
+  public final static int BUTTONS_COLUMN_SPAN = 5;
+  public final static int BUTTONS_ROW_SPAN = 1;
+
+  public final static int SLIDER_COLUMN = 3;
+  public final static int SLIDER_ROW = 4;
+
+  public final static int SLIDER_COLUMN_SPAN = 2;
+  public final static int SLIDER_ROW_SPAN = 1;
 
   public GUIContainer(Stage primaryStage, String language, Config config, SimulationController simulationEngine) {
     pane = new GridPane();
@@ -113,27 +142,27 @@ public class GUIContainer {
     grid = new RectangleVisualGrid(DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE, GRID_SIZE, scene);
 //    grid.updateGrid(5,5);
     pane.getChildren().add(grid.getGridLayout());
-    GridPane.setConstraints(grid.getGridLayout(), 0, 0, 3, 4);
+    GridPane.setConstraints(grid.getGridLayout(), GRID_COLUMN, GRID_ROW, GRID_COLUMN_SPAN, GRID_ROW_SPAN);
   }
 
   private void setUpDropDown(List<String> FileNames) {
     drop = new DropDown(FileNames, myResources.getString("DropButton"));
     drop.getButton().setOnAction(e -> saveCommand(drop.getButton().getText()));
     pane.getChildren().add(drop.getContainer());
-    GridPane.setConstraints(drop.getContainer(), 3, 1, 2, 1);
+    GridPane.setConstraints(drop.getContainer(), DROP_DOWN_COLUMN, DROP_DOWN_ROW, DROP_DOWN_COLUMN_SPAN, DROP_DOWN_ROW_SPAN);
   }
 
   private void setUpFileSaver() {
     FileSaver save = new FileSaver(myResources.getString("Save"), grid);
     pane.getChildren().add(save.getButton());
-    GridPane.setConstraints(save.getButton(), 4, 0);
+    GridPane.setConstraints(save.getButton(), FILE_SAVER_COLUMN, FILE_SAVER_ROW);
     save.setFile("Test");
   }
 
   private void setUpFileUploader(Config config) {
     uploader = new FileUploader(myResources.getString("Upload"), config);
     pane.getChildren().add(uploader.getButton());
-    GridPane.setConstraints(uploader.getButton(), 3, 0);
+    GridPane.setConstraints(uploader.getButton(), FILE_UPLOADER_COLUMN, FILE_UPLOADER_ROW);
   }
 
   private void SetUpDescriptionBox() {
@@ -141,9 +170,8 @@ public class GUIContainer {
     description.setId("TextBox");
     VBox descriptionContainer = new VBox();
     descriptionContainer.getChildren().add(description);
-//    descriptionContainer.setVgrow(description, Priority.ALWAYS);
     pane.getChildren().add(descriptionContainer);
-    GridPane.setConstraints(descriptionContainer, 3, 3, 2, 1);
+    GridPane.setConstraints(descriptionContainer, DESCRIPTION_BOX_COLUMN, DESCRIPTION_BOX_ROW, DESCRIPTION_BOX_COLUMN_SPAN, DESCRIPTION_BOX_ROW_SPAN);
   }
 
   private void setUpButtons(SimulationController simulationEngine) {
@@ -159,7 +187,7 @@ public class GUIContainer {
 
     //https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/GridPane.html
     pane.getChildren().add(buttons.getContainer());
-    GridPane.setConstraints(buttons.getContainer(), 0,5, 5, 1);
+    GridPane.setConstraints(buttons.getContainer(), BUTTONS_COLUMN,BUTTONS_ROW, BUTTONS_COLUMN_SPAN, BUTTONS_ROW_SPAN);
   }
 
   public void saveCommand(String string) {
@@ -168,9 +196,9 @@ public class GUIContainer {
   }
 
   public void setUpSliderContainer() {
-    slider = new SliderContainer(0, 4, 1, 1, myResources.getString("SliderCaption"));
+    slider = new SliderContainer(myResources.getString("SliderCaption"));
     pane.getChildren().add(slider.getContainer());
-    GridPane.setConstraints(slider.getContainer(), 3, 4, 2, 1);
+    GridPane.setConstraints(slider.getContainer(), SLIDER_COLUMN, SLIDER_ROW, SLIDER_COLUMN_SPAN, SLIDER_ROW_SPAN);
   }
 
   public void updateSliderValue() {
