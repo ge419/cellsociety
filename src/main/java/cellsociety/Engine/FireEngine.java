0db3dea92f5a30761a39f61bb39ded3a47338d6c
Changmin Shin <cs521@duke.edu>
2023-02-12 00:31:29 -0500

    added engines for all simulations, setup FireEngine

---
+++ src/main/java/cellsociety/Engine/FireEngine.java
@@ -0,0 +1,40 @@
+package cellsociety.Engine;
+
+import cellsociety.GUI.VisualGrid;
+import cellsociety.Grid;
+import java.util.HashMap;
+
+/**
+ * @author Changmin Shin
+ */
+
+public class FireEngine extends SimEngine {
+
+  private static final String FIRE_EMPTY = NAMES_FILE.getString("FireEmpty");
+  private static final String FIRE_TREE = NAMES_FILE.getString("FireTree");
+  private static final String FIRE_BURNING = NAMES_FILE.getString("FireBurning");
+
+  public FireEngine(VisualGrid visualGrid, String initState, Grid grid, Grid initGrid,
+      HashMap<String, Double> params)
+      throws Exception {
+    super(visualGrid, initState, grid, initGrid, params);
+    // setup initial state grid for both updating grid and initial grid
+    listToGrid(strToGrid(initState), grid);
+    listToGrid(strToGrid(initState), initGrid);
+  }
+
+  @Override
+  String statusIntToStr(int status) throws Exception {
+    if (status == 0) {
+      return FIRE_EMPTY;
+    } else if (status == 1) {
+      return FIRE_TREE;
+    } else if (status == 2) {
+      return FIRE_BURNING;
+    } else {
+      //TODO: create a new exception class, also return e
+      throw new Exception("Invalid input for status");
+    }
+  }
+}
+
---
+++ src/main/java/cellsociety/Engine/WatorEngine.java
@@ -0,0 +1,5 @@
+package cellsociety.Engine;
+
+public class WatorEngine {
+
+}
