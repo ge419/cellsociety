3f7e05fc56ed1d0ac76ae504eb5e50e910ee1892
Han Zhang <hz244@duke.edu>
2023-02-11 22:43:09 -0500

    REFACTOR: Moved the function to send selected Files from Main.java to the DropDown.java to make it an lambda expression for the button in DropDown.java

--- src/main/java/cellsociety/GUI/DropDown.java
+++ src/main/java/cellsociety/GUI/DropDown.java
@@ -1,6 +1,8 @@
 package cellsociety.GUI;
 
 import cellsociety.Config;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -10,10 +12,8 @@ public class DropDown {
 
   private static ComboBox<String> dropdown;
 
-  private static Button button;
-
   private static VBox container;
-  public DropDown(List<String> list, String buttonlabel, Config config) {
+  public DropDown(List<String> list, String buttonLabel, Config config) {
 
     container = new VBox();
     container.setId("Container-Vbox");
@@ -22,23 +22,50 @@ public class DropDown {
     dropdown.setValue("Selected a Choice");
 
     container.getChildren().add(dropdown);
-
-    button = new Button(buttonlabel);
+    Button button = new Button(buttonLabel);
     button.setId("drop-button");
-    button.setOnAction(e -> loadFile());
-
+    button.setOnAction(e -> loadFile(config));
     container.getChildren().add(button);
-
   }
 
-  private void loadFile() {
+  private void loadFile(Config config) {
+    sendFileToConfig(dropdown.getValue(), config);
+  }
+
+  private void sendFileToConfig(String fileName, Config config) {
+    List<String> dirctNames = new ArrayList<>();
+    dirctNames.add("data/Preloaded_Files");
+    File file = new File("");
+    while (!dirctNames.isEmpty()) {
+      String dirctName = dirctNames.get(dirctNames.size() - 1);
+      dirctNames.remove(dirctNames.size() - 1);
+      file = findFile(fileName, dirctName);
+      if (file != null) {
+        break;
+      }
+    }
+    config.readFile(file);
+  }
+
+  /**
+   * This method is based on a CHAT GPT conversation, https://sharegpt.com/c/vSTdS6B, finds a file
+   * based on inputted fileName
+   *
+   * @param fileName This is the name of the XML file
+   * @return The return value is the file that is the file name
+   */
+  private File findFile(String fileName, String directoryName) {
+    File file = new File(directoryName);
+    File[] matchingFiles = file.listFiles((dir, name) -> name.endsWith(fileName));
+    assert matchingFiles != null;
+    if (matchingFiles.length == 0) {
+      return null;
+    }
+    return matchingFiles[0];
   }
 
   public String getValue(){
     return dropdown.getValue();
-  }
-
-  public Button getButton(){ return button;
   }
   public VBox getContainer(){
     return container;
